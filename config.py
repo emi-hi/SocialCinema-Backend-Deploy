8d2865e61398f7d360d66cd5a9e5366505fef97d
Emily <emily.hillier@gmail.com>
2019-12-04 12:26:52 -0800

    set up everything for heroku

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ import dotenv
 dotenv.load_dotenv()
 DB_NAME = os.getenv('DB_NAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+SQLALCHEMY_DATABASE = os.getenv('heroku_db')
+
 
 class Config:
   TESTING = environ.get('TESTING')
   FLASK_DEBUG = environ.get('FLASK_DEBUG')
-
-  # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://development:development@localhost/social_cinema'
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@localhost/social_cinema'.format(DB_NAME, DB_PASSWORD)
+  SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE
   SQLALCHEMY_TRACK_MODIFICATIONS = False
